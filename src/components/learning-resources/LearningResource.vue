67bc1a80a85ef5547f78bad44219d50f1a65ded0
muteeburrehman <muteeb285@gmail.com>
2023-10-07 21:20:38 +0500

    Updated LearningResource Component

--- src/components/learning-resources/LearningResource.vue
+++ src/components/learning-resources/LearningResource.vue
@@ -3,7 +3,7 @@
       <base-card>
         <header>
           <h3>{{ title }}</h3>
-          <button>Delete</button>
+          <base-button mode='flat' @click="deleteResource(id)">Delete</base-button>
         </header>
       
       <p>{{ description }}</p>
@@ -17,9 +17,11 @@
   <script>
 
 
+
   export default {
   
-    props: ['title', 'description', 'link']
+    props: ['id','title', 'description', 'link'],
+    inject:['deleteResource']
   }
   </script>
 
